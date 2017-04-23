3230816df8b289c48b42302c5d7199ef6b74729b
Rudolf Schmidt <me@rudionrails.com>
2017-04-23 17:32:50 +0200

    Adds event listener for ESC key

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom';
 import Search from './components/pages/Search';
 import './index.css';
 
-function destroy() {
-  alert('destroy');
+const escEventListener = ({ keyCode }) => {
+  if(keyCode === 27) destroy();
 };
+
+function destroy() {
+  document.removeEventListener('keydown', escEventListener);
+};
+
+document.addEventListener('keydown', escEventListener);
 
 ReactDOM.render(
   <Search onClose={destroy} />,
