b4d8fdb14ed7aab0997446d3ec81f7d31b325dbc
Rudolf Schmidt <me@rudionrails.com>
2017-05-09 15:20:56 +0200

    Cleanup for Results bootstrap 3

--- src/components/organisms/Results.js
+++ src/components/organisms/Results.js
@@ -2,19 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Section from 'src/components/molecules/Section';
-import './Results.css';
 
 const Results = ({
   className,
   results,
 }) => (
-  <div className={`Results container-fluid ${className}`}>
-    <div className="row">
-      {results.map(result =>
-        <Section
-          key={`section-${result.title}`}
-          { ...result }
-        />)}
+  <div className={`Results ${className}`}>
+    <div class="container-fluid">
+      <div className="row">
+        {results.map(result =>
+          <Section
+            key={`section-${result.title}`}
+            { ...result }
+          />)}
+      </div>
     </div>
   </div>
 );
