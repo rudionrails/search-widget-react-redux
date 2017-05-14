278ea60ee5a3e2e1c524675db68bf309b402d2d2
Rudolf Schmidt <me@rudionrails.com>
2017-05-14 15:41:26 +0200

    Updates Search test

--- src/components/pages/Search.test.js
+++ src/components/pages/Search.test.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import td from 'testdouble';
+import { render, shallow } from 'enzyme';
 import Search from './Search';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Search onClose={() => true} />, div);
+// isLoading: PropTypes.bool,
+// query: PropTypes.string,
+
+const props = {
+  results: [],
+  onSearch: () => {},
+  onClose: () => {},
+};
+
+test('renders without crashing', () => {
+  render(<Search { ...props } />);
+});
+
+test('renders Bar', () => {
+  const bar = shallow(<Search { ...props } />).find('Bar');
+  expect(bar.length).toBe(1);
+});
+
+test('renders Results', () => {
+  const results = shallow(<Search { ...props } />).find('Results');
+  expect(results.length).toBe(1);
+});
+
+test('triggers onClose when pressing the close button', () => {
+  const onClose = td.function('onClose');
+  const search = shallow(<Search { ...props } onClose={onClose} />);
+  const closeButton = search.find('.Search-close');
+
+  closeButton.simulate('click');
+  td.verify(onClose());
 });
