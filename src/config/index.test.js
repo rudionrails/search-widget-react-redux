65418b09aa5ef2a8a8915a0b84c263bf14df4d6a
Rudolf Schmidt <me@rudionrails.com>
2017-05-21 18:21:08 +0200

    Updates ap configuration

---
+++ src/config/index.test.js
@@ -0,0 +1,32 @@
+import td from 'testdouble';
+
+// module under test
+import config, { configure } from './index';
+
+test('exports a default config object', () => {
+  expect(typeof config).toBe('object');
+});
+
+test('export a configure function', () => {
+  expect(typeof configure).toBe('function');
+});
+
+describe('configure', () => {
+  afterEach(() => {
+    td.reset();
+  });
+
+  test('assigns value to config', () => {
+    const assign = td.replace(Object, 'assign');
+    configure({ foo: 'bar' });
+  
+    td.verify(assign(config, { foo: 'bar' }));
+  });
+
+  test('does not assign value when undefined', () => {
+    const assign = td.replace(Object, 'assign');
+
+    configure({ foo: undefined });
+    td.verify(assign(), {times: 0, ignoreExtraArgs: true});
+  });
+});
